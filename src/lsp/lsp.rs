0986673f6712f30c5c4c541bc29a483b174efdd2
Thomas <bdachai308@gmail.com>
2024-08-01 23:38:50 +0700

    Stderr handling

--- src/lsp/lsp.rs
+++ src/lsp/lsp.rs
@@ -12,7 +12,10 @@ use lsp_types::{CodeActionKind, ServerCapabilities};
 use parking_lot::{Mutex, RwLock};
 use serde::{Deserialize, Serialize};
 use serde_json::{value::RawValue, Value};
-use tokio::process::{Child, ChildStderr, ChildStdin, ChildStdout};
+use tokio::{
+    io::{AsyncBufReadExt, BufReader},
+    process::{Child, ChildStderr, ChildStdin, ChildStdout},
+};
 
 const JSON_RPC_VER: &str = "2.0";
 const CONTENT_LENGTH_HEADERS: &str = "Content-Length: ";
@@ -170,6 +173,19 @@ impl LanguageServer {
         working_dir: &Path,
         code_action_kinds: Option<Vec<CodeActionKind>>,
     ) -> Self {
+        // return an outbound channels for proccessing
+        let (outbound_tx, outbound_rx) = tokio::sync::mpsc::unbounded_channel();
+        let (output_done_tx, output_done_rx) = tokio::sync::oneshot::channel();
+        // init all the handlers
+        let notification_handlers =
+            Arc::new(Mutex::new(HashMap::<_, NotificaionHandler>::default()));
+        let response_handlers =
+            Arc::new(Mutex::new(Some(HashMap::<_, ResponseHandler>::default())));
+        let io_handers = Arc::new(Mutex::new(HashMap::default()));
+
+        //spawn stdout handling task
+        //spawn stderr task
+        //spawn output task
     }
 
     fn initialize() {
@@ -206,9 +222,51 @@ impl LanguageServer {
 
     fn handle_stdin() {}
 
-    fn handle_stderr() {}
+    async fn handle_stderr(
+        stderr: ChildStderr,
+        io_handers: Arc<Mutex<HashMap<i32, IoHandler>>>,
+        stderr_capture: Arc<Mutex<Option<String>>>,
+    ) -> anyhow::Result<()> {
+        let mut stderr_bufreader = BufReader::new(stderr);
+        let mut buffer = Vec::new();
 
-    fn handle_stdout() {}
+        loop {
+            buffer.clear();
+
+            let byte_reads = stderr_bufreader.read_until(b'\n', &mut buffer).await?;
+            if byte_reads == 0 {
+                return Ok(());
+            };
+
+            if let Ok(message) = std::str::from_utf8(&buffer) {
+                for handler in io_handers.lock().values_mut() {
+                    handler(IoKind::StdErr, message);
+                }
+                if let Some(stderr) = stderr_capture.lock().as_mut() {
+                    stderr.push_str(message);
+                }
+            }
+            tokio::task::yield_now();
+        }
+    }
+
+    fn handle_stdout(
+        stdout: ChildStdout,
+        notification_handlers: Arc<Mutex<HashMap<&'static str, NotificaionHandler>>>,
+        response_handlers: Arc<Mutex<Option<HashMap<RequestId, ResponseHandler>>>>,
+        io_handers: Arc<Mutex<HashMap<i32, IoHandler>>>,
+    ) {
+        let stdout = BufReader::new(stdout);
+        let _clear_response_handlers = {
+            let response_handlers = response_handlers.clone();
+            move || {
+                response_handlers.lock().take();
+            }
+        };
+
+        //TODO: Create input handlers
+        let mut input_handler;
+    }
 
     fn handle_request() {
         println!("Register a handler for upcoming requests")
@@ -233,4 +291,8 @@ impl LanguageServer {
     fn shutdown() {
         println!("Prepare to drop the server")
     }
+}
+pub struct Deferred<F: FnOnce()>(Option<F>);
+pub fn defer<F: FnOnce()>(f: F) -> Deferred<F> {
+    Deferred(Some(f))
 }
