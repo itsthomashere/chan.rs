ea4c92c8133b853cfc510f86ce2a260a91b690a2
Thomas <bdachai308@gmail.com>
2024-08-05 10:41:46 +0700

    Moving definitions over to types

--- src/lsp/lsp.rs
+++ src/lsp/lsp.rs
@@ -1,72 +1,114 @@
-use std::io::Write;
-use std::{
-    collections::HashMap,
-    ffi::OsString,
-    path::{Path, PathBuf},
-    process::Stdio,
-    sync::Arc,
-};
+pub mod types;
 
-use anyhow::{anyhow, Ok};
+use crate::types::types::LanguageServerBinary;
+use std::collections::HashMap;
+use std::io::Write;
+use std::path::PathBuf;
+use std::process::Stdio;
+use std::{path::Path, sync::Arc};
+
+use anyhow::{anyhow, Context, Ok};
 use lsp_types::{
     request::{self, Request},
     InitializeParams,
 };
-use parking_lot::Mutex;
-use serde::{Deserialize, Serialize};
-use tokio::io::{AsyncBufReadExt, BufReader};
-use tokio::process::ChildStdout;
-use tokio::{
-    io::{AsyncWriteExt, BufWriter},
-    process::{Child, Command},
+use lsp_types::{CodeActionKind, ServerCapabilities};
+use parking_lot::{Mutex, RwLock};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::process::{Child, Command};
+use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
+use tokio::{io::BufWriter, process::ChildStdout};
+use types::types::{
+    LspRequest, LspRequestId, NotificationHandler, ResponseHandler, CONTENT_LEN_HEADER,
+    HEADER_DELIMITER, JSONPRC_VER,
 };
 
-pub const CONTENT_LEN_HEADER: &str = "Content-Length: ";
-pub const JSONPRC_VER: &str = "2.0";
-const HEADER_DELIMITER: &[u8; 4] = b"\r\n\r\n";
-pub struct LanguageServerBinary {
-    pub path: PathBuf,
-    pub envs: Option<HashMap<String, String>>,
-    pub args: Vec<OsString>,
-}
-
 pub struct LanguageSeverProcess {
-    pub process: Arc<Mutex<Child>>,
+    name: Arc<str>,
+    process: Arc<Mutex<Option<Child>>>,
+    capabilities: RwLock<ServerCapabilities>,
+    response_handlers: Arc<Mutex<Option<HashMap<LspRequestId, ResponseHandler>>>>,
+    notification_handlers: Arc<Mutex<HashMap<&'static str, NotificationHandler>>>,
+    code_action_kinds: Option<Vec<CodeActionKind>>,
+    outbound_sender: UnboundedSender<String>,
+    outbound_receiver: UnboundedReceiver<String>,
+    root_path: PathBuf,
+    working_dir: PathBuf,
 }
-
 impl LanguageSeverProcess {
-    pub fn new(binary: LanguageServerBinary, root_path: &Path) -> Self {
-        let root_dir = if root_path.is_dir() {
+    pub fn new(
+        binary: LanguageServerBinary,
+        root_path: &Path,
+        code_action_kinds: Option<Vec<CodeActionKind>>,
+    ) -> anyhow::Result<Self> {
+        let working_dir = if root_path.is_dir() {
             root_path
         } else {
-            Path::new("/")
+            root_path.parent().unwrap_or_else(|| Path::new("/"))
         };
 
         let mut command = Command::new(&binary.path);
         command
-            .current_dir(root_dir)
-            .args(&binary.args)
+            .current_dir(working_dir)
             .envs(binary.envs.unwrap_or_default())
+            .args(&binary.args)
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
             .stderr(Stdio::piped())
             .kill_on_drop(true);
 
-        let server = command.spawn().unwrap();
+        let process = command.spawn().with_context(|| {
+            format!(
+                "failed to spawn command. path: {:?}, working directory: {:?}, args: {:?}",
+                binary.path, working_dir, &binary.args
+            )
+        })?;
+
+        let mut server =
+            Self::binding_backend(Some(process), root_path, working_dir, code_action_kinds);
+
+        if let Some(name) = binary.path.file_name() {
+            server.name = name.to_string_lossy().into()
+        };
+
+        Ok(server)
+    }
+
+    fn binding_backend(
+        process: Option<Child>,
+        root_path: &Path,
+        working_dir: &Path,
+        code_action_kinds: Option<Vec<CodeActionKind>>,
+    ) -> Self {
+        let notification_handlers =
+            Arc::new(Mutex::new(HashMap::<_, NotificationHandler>::default()));
+        let response_handlers =
+            Arc::new(Mutex::new(Some(HashMap::<_, ResponseHandler>::default())));
+
+        let (outbound_sender, outbound_receiver) = mpsc::unbounded_channel::<String>();
 
         Self {
-            process: Arc::new(Mutex::new(server)),
+            name: Arc::default(),
+            process: Arc::new(Mutex::new(process)),
+            capabilities: Default::default(),
+            response_handlers,
+            notification_handlers,
+            code_action_kinds,
+            outbound_sender,
+            outbound_receiver,
+            root_path: root_path.to_path_buf(),
+            working_dir: working_dir.to_path_buf(),
         }
     }
 
-    pub async fn initialize(&mut self) -> anyhow::Result<()> {
-        let mut server = self.process.lock();
+    pub async fn initialize(self) -> anyhow::Result<()> {
+        let mut server = self.process.lock().unwrap();
         let stdin = server.stdin.take().unwrap();
 
         let params = InitializeParams::default();
         let message = serde_json::to_string(&LspRequest {
             jsonprc: JSONPRC_VER,
-            id: 0,
+            id: LspRequestId::Int(0),
             method: request::Initialize::METHOD,
             params,
         })
@@ -86,14 +128,8 @@ impl LanguageSeverProcess {
 
         Ok(())
     }
-}
 
-#[derive(Debug, Deserialize, Serialize)]
-struct LspRequest<'a, T> {
-    jsonprc: &'a str,
-    id: i32,
-    method: &'a str,
-    params: T,
+    pub fn request<T: request::Request>(&self, params: T::Params) {}
 }
 
 pub async fn read_headers(
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,14 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use anyhow::anyhow;
-use liblspc::{LanguageServerBinary, LanguageSeverProcess, CONTENT_LEN_HEADER};
-use lsp_types::{InitializeResult, ServerInfo};
+use anyhow::{anyhow, Result};
+use liblspc::{read_headers, LanguageServerBinary, LanguageSeverProcess, CONTENT_LEN_HEADER};
 use serde::{Deserialize, Serialize};
-use serde_json::value::RawValue;
+use serde_json::{json, value::RawValue};
 use tokio::io::{AsyncBufReadExt, AsyncReadExt, BufReader};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let binary = LanguageServerBinary {
         path: PathBuf::from(OsString::from("rust-analyzer")),
         envs: None,
@@ -19,56 +18,40 @@ async fn main() {
     };
     let root = Path::new("/");
 
-    let mut procc = LanguageSeverProcess::new(binary, root);
+    let mut procc = LanguageSeverProcess::new(binary, root, None);
     let stdout = procc.process.lock().stdout.take().unwrap();
+
+    let mut reader = BufReader::new(stdout);
+    let mut buffer: Vec<u8> = Vec::new();
     {
         procc.initialize().await.unwrap()
     }
 
-    let mut reader = BufReader::new(stdout);
-    let mut buffer: Vec<u8> = Vec::new();
     loop {
-        if let Ok(message) = reader.read_until(b'\n', &mut buffer).await {
-            let headers = std::str::from_utf8(&buffer).unwrap_or_default();
+        buffer.clear();
+        read_headers(&mut reader, &mut buffer).await?;
+        let headers = std::str::from_utf8(&buffer)?;
+        let message_len: usize = headers
+            .split('\n')
+            .find(|line| line.starts_with(CONTENT_LEN_HEADER))
+            .and_then(|line| line.strip_prefix(CONTENT_LEN_HEADER))
+            .ok_or_else(|| anyhow!("Invalid lsp headers"))?
+            .trim_end()
+            .parse()?;
 
-            let message_len = headers
-                .split('\n')
-                .find(|line| line.starts_with(CONTENT_LEN_HEADER))
-                .and_then(|line| line.strip_prefix(CONTENT_LEN_HEADER))
-                .ok_or_else(|| anyhow!("Failed to parse headerse"))
-                .unwrap()
-                .trim_end()
-                .parse()
-                .unwrap();
+        buffer.resize(message_len, 0);
+        reader.read_exact(&mut buffer).await?;
 
-            buffer.resize(message_len, 0);
-
-            reader.read_exact(&mut buffer).await.unwrap();
-            let buf2 = buffer.to_vec();
-            println!("{}", std::str::from_utf8(&buffer).unwrap());
-            let message_json: Response<InitializeResult> = serde_json::from_slice(&buf2).unwrap();
-            println!("{:?}", message_json)
-        };
+        if let Ok(AnyResponse {
+            jsonrpc,
+            id,
+            error,
+            result,
+            ..
+        }) = serde_json::from_slice(&buffer)
+        {
+            println!("{:?}", error);
+            println!("{}", result.unwrap());
+        }
     }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Response<'a, T> {
-    jsonrpc: &'a str,
-    id: i32,
-    #[serde(flatten)]
-    value: LspResult<T>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "snake_case")]
-enum LspResult<T> {
-    #[serde(rename = "result")]
-    Ok(Option<T>),
-    Error(Option<Error>),
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Error {
-    message: String,
 }
