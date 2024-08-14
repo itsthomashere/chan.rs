aa3a68b71cd1bd650f16bb81ee678dd5d6e582ff
BMHDAC <bdachai308@gmail.com>
2024-08-14 21:12:01 +0700

    Add owned channels to listener

--- src/lsp/listener/listener.rs
+++ src/lsp/listener/listener.rs
@@ -13,8 +13,8 @@ use tokio::sync::{
 };
 
 use crate::types::types::{
-    AnyNotification, AnyResponse, InternalLspRequest, LspRequest, LspRequestFuture, LspRequestId,
-    NotificationHandler, ResponseHandler, JSONPRC_VER,
+    AnyNotification, AnyResponse, InternalLspRequest, LspNotification, LspRequest,
+    LspRequestFuture, LspRequestId, NotificationHandler, ResponseHandler, JSONPRC_VER,
 };
 
 pub(crate) struct Listener {
@@ -22,11 +22,12 @@ pub(crate) struct Listener {
     response_handlers: Arc<Mutex<Option<HashMap<LspRequestId, ResponseHandler>>>>,
     notification_handlers: Arc<Mutex<HashMap<&'static str, NotificationHandler>>>,
     request_tx: UnboundedSender<String>,
+    response_rx: UnboundedReceiver<String>,
     output_tx: UnboundedSender<String>,
 }
 
 impl Listener {
-    async fn new(
+    pub(crate) fn new(
         response_rx: UnboundedReceiver<String>,
         request_tx: UnboundedSender<String>,
         output_tx: UnboundedSender<String>,
@@ -36,32 +37,24 @@ impl Listener {
         )));
         let notification_handlers =
             Arc::new(Mutex::new(HashMap::<_, NotificationHandler>::default()));
-        Self::response_listener(
-            response_rx,
-            response_handlers.clone(),
-            notification_handlers.clone(),
-        )
-        .await?;
+
         Ok(Self {
             next_id: Default::default(),
             response_handlers,
             notification_handlers,
             request_tx,
             output_tx,
+            response_rx,
         })
     }
 
-    async fn response_listener(
-        mut response_rx: UnboundedReceiver<String>,
-        response_handlers: Arc<Mutex<Option<HashMap<LspRequestId, ResponseHandler>>>>,
-        notification_handlers: Arc<Mutex<HashMap<&'static str, NotificationHandler>>>,
-    ) -> anyhow::Result<()> {
-        while let Some(msg) = response_rx.recv().await {
+    pub(crate) async fn response_listener(&mut self) -> anyhow::Result<()> {
+        while let Some(msg) = self.response_rx.recv().await {
             if let Ok(AnyResponse {
                 id, error, result, ..
             }) = serde_json::from_str(&msg)
             {
-                let mut response_handlers = response_handlers.lock();
+                let mut response_handlers = self.response_handlers.lock();
                 if let Some(handler) = response_handlers
                     .as_mut()
                     .and_then(|handler| handler.remove(&id))
@@ -78,7 +71,7 @@ impl Listener {
             } else if let Ok(AnyNotification { id, method, params }) =
                 serde_json::from_str::<AnyNotification>(&msg)
             {
-                let mut notification_handlers = notification_handlers.lock();
+                let mut notification_handlers = self.notification_handlers.lock();
                 if let Some(mut handler) = notification_handlers.remove(method.as_str()) {
                     drop(notification_handlers);
                     if let Some(params) = params {
@@ -92,17 +85,17 @@ impl Listener {
         Ok(())
     }
 
-    async fn send_request<T: request::Request>(
-        next_id: &AtomicI32,
-        request_tx: UnboundedSender<String>,
-        response_handlers: Arc<Mutex<Option<HashMap<LspRequestId, ResponseHandler>>>>,
+    pub(crate) async fn send_request<T: request::Request>(
+        &self,
         params: T::Params,
     ) -> impl LspRequestFuture<anyhow::Result<T::Result>>
     where
         T::Result: 'static + Send,
     {
-        let request_tx = request_tx.clone();
-        let id = next_id.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+        let request_tx = self.request_tx.clone();
+        let id = self
+            .next_id
+            .fetch_add(1, std::sync::atomic::Ordering::SeqCst);
         let message = serde_json::to_string(&InternalLspRequest {
             jsonrpc: JSONPRC_VER,
             id: LspRequestId::Int(id),
@@ -111,7 +104,8 @@ impl Listener {
         })
         .unwrap();
         let (tx, mut rx) = oneshot::channel();
-        let handle_response = response_handlers
+        let handle_response = self
+            .response_handlers
             .lock()
             .as_mut()
             .ok_or_else(|| anyhow!("Server shutdown"))
@@ -119,7 +113,7 @@ impl Listener {
                 handler.insert(
                     LspRequestId::Int(id),
                     Box::new(move |result| {
-                        let _ = tokio::spawn(async move {
+                        tokio::spawn(async move {
                             let response = match result {
                                 Ok(response) => match serde_json::from_str(&response) {
                                     Ok(deserialize) => Ok(deserialize),
@@ -146,11 +140,26 @@ impl Listener {
             handle_response.unwrap_or_default();
             send.unwrap_or_default();
             match rx.try_recv() {
-                Ok(response) => return response,
-                Err(err) => return Err(err.into()),
-            };
+                Ok(response) => response,
+                Err(err) => Err(err.into()),
+            }
         })
     }
 
-    async fn send_notification<T: notification::Notification>() {}
+    async fn send_notification<T: notification::Notification>(
+        &self,
+        params: T::Params,
+    ) -> anyhow::Result<()> {
+        let message = serde_json::to_string(&LspNotification {
+            jsonrpc: JSONPRC_VER,
+            method: T::METHOD,
+            params,
+        })
+        .unwrap();
+        let tx = self.request_tx.clone();
+        tx.send(message);
+        tx.downgrade();
+
+        Ok(())
+    }
 }
